1c30e6a36871d3a75ccfcce21ae2a76c4cc6dad0
LucasBoni29 <santosboni2002@gmail.com>
2023-05-31 22:01:51 -0300

    AnimeFlix - Adicionado API dos animes

--- src/components/CardAnime.jsx
+++ src/components/CardAnime.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 export default function CardAnime({anime}) {
     const [favorito, setFavorito] = useState(false)
+    const poster = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/" + anime.poster_path
 
     return (
         <div className="flex flex-col p-3 w-56">
@@ -20,14 +21,14 @@ export default function CardAnime({anime}) {
                         onClick={ () => setFavorito(true)}
                     />
                 }
-                <img className="rounded-lg" src={anime.poster} />
+                <img className="rounded-lg" src={poster} />
             </div>
-            <h4 className="text-xl my-2 line-clamp-1 w-fit ">{anime.titulo}</h4>
+            <h4 className="text-xl my-2 line-clamp-1 w-fit ">{anime.name}</h4>
             <div className="flex justify-center items-center">
                 <StarIcon className='text-amber-400' />
-                <span className="text-lg">{anime.nota}</span>
+                <span className="text-lg">{anime.vote_average}</span>
             </div>
-            <a href="#" className="rounded-lg bg-cyan-700 py-2 text-center ">detalhes</a>
+            <a href="#" className="rounded-lg bg-cyan-700 py-2 text-center ">Detalhes</a>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,31 @@ import CardAnime from "@/components/CardAnime";
 import Header from "@/components/Header";
 import SecaoAnimes from "@/components/SecaoAnimes";
 import Titulo from "@/components/Titulo";
+import { useState } from "react";
 
 export default function Home() {
 
-  const animes = [
-    {
-      titulo: "Jujutso Kaisen",
-      nota: "8.9",
-      poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/kzYbNJ4JaQiRny0cDi8a6bNX5w3.jpg"
-    },
-    {
-      titulo: "Dr. Stone",
-      nota: "8.9",
-      poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/kzYbNJ4JaQiRny0cDi8a6bNX5w3.jpg"
-    },
-    {
-      titulo: "Kimetsu no Yaiba",
-      nota: "8.9",
-      poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/kzYbNJ4JaQiRny0cDi8a6bNX5w3.jpg"
-    },
-    {
-      titulo: "One Piece",
-      nota: "8.9",
-      poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/kzYbNJ4JaQiRny0cDi8a6bNX5w3.jpg"
-    },
-    {
-      titulo: "Bleach",
-      nota: "6.7",
-      poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/5OeWHBMifiRPMz6eS23miS5ttED.jpg"
+  const [animes, setAnimes] = useState([]);
+  const [animesLancamento, setAnimesLancamento] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTkyMjY2NzQ4MWFiMjA3ZDY0MjQ1MGIwZWZiNDYxZSIsInN1YiI6IjVlYTA5ZTZiYmU0YjM2MDAxYzU5NWExNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vhu0pPCiIwmtrpyOHdBlQid8HJJllaHthn1MERS_ANg'
     }
-  ]
+  };
+  
+  fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=16,10759,10765', options)
+    .then(response => response.json())
+    .then(response => setAnimes(response.results))
+    .catch(err => console.error(err));
 
   return (
     <> 
       <Header />
       <div>
-        <img className="h-72 brightness-50 object-cover w-screen" src="https://www.themoviedb.org/t/p/original/cSWkX0fTjrBCU2sbEmyRFd1SxPA.jpg" />
+        <img className="h-72 brightness-50 object-cover w-screen" src="https://www.themoviedb.org/t/p/original/6VJy1E7PcgjKj6pZjqTVPr8CFal.jpg" />
       </div>
       
       <main className="flex min-h-screen flex-col px-24">
@@ -48,9 +37,10 @@ export default function Home() {
         <Titulo>Melhores animes do momento</Titulo>
         <Titulo>Lançamentos</Titulo>
 
+        <SecaoAnimes animes={animesLancamento}/>
 
       </main> //JSX
     </>
-
   )
+  
 }
